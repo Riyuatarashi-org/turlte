bf92780229a2797cf20c7a9736ab1ae2849636d4
moeetomoni <dev@moee.fr>
2023-02-04 11:11:48 +0100

    cde-feat(cs-fixer): update cs-fixer rules

--- tools/php-cs-fixer/php-cs-fixer-rules.php
+++ tools/php-cs-fixer/php-cs-fixer-rules.php
@@ -4,44 +4,39 @@ return [
     '@PHP82Migration' => true,
     '@PhpCsFixer'     => true, // includes @Symfony
 
+    'array_syntax' => [ 'syntax' => 'short' ],
+
     'blank_line_before_statement' => [
         'statements' => [
             'continue', 'declare', 'do', 'for', 'foreach', 'if', 'return', 'switch', 'throw', 'try', 'while',
         ],
     ],
 
-    'class_definition' => [ 'multi_line_extends_each_single_line' => true ],
+    'class_definition'     => [ 'multi_line_extends_each_single_line' => true ],
+    'class_keyword_remove' => false,
 
+    'combine_consecutive_issets' => true,
     'combine_consecutive_unsets' => true,
+
+    'compact_nullable_typehint' => true,
 
     'concat_space' => [ 'spacing' => 'one' ],
 
-    'multiline_whitespace_before_semicolons' => false,
+    'constant_case' => true,
 
-    'no_superfluous_elseif'      => true,
-    'no_superfluous_phpdoc_tags' => true,
+    'declare_equal_normalize' => [ 'space' => 'single' ],
+    'declare_strict_types'    => true,
 
-    'not_operator_with_successor_space' => true,
+    'dir_constant' => true,
 
-    'no_empty_comment' => true,
+    'doctrine_annotation_array_assignment' => true,
+    'doctrine_annotation_braces'           => true,
+    'doctrine_annotation_indentation'      => true,
+    'doctrine_annotation_spaces'           => true,
 
-    'no_unset_on_property' => true,
+    'explicit_indirect_variable' => true,
 
-    'phpdoc_add_missing_param_annotation' => false,
-
-    'phpdoc_annotation_without_dot' => true,
-
-    'phpdoc_to_comment' => true,
-
-    'return_assignment' => true,
-
-    'strict_comparison' => true,
-
-    'yoda_style' => [
-        'equal'            => false,
-        'identical'        => false,
-        'less_and_greater' => false,
-    ],
+    'function_typehint_space' => true,
 
     'global_namespace_import' => [
         'import_classes'   => true,
@@ -49,12 +44,49 @@ return [
         'import_functions' => true,
     ],
 
-    'declare_strict_types' => true,
+    'heredoc_to_nowdoc' => true,
+
+    'lowercase_cast'     => true,
+    'lowercase_keywords' => true,
+
+    'magic_constant_casing' => true,
+
+    'modernize_types_casting' => true,
+
+    'multiline_whitespace_before_semicolons' => false,
+
+    'native_function_casing' => true,
+
+    'new_with_braces' => true,
+
+    'no_blank_lines_after_class_opening' => true,
+    'no_blank_lines_after_phpdoc'        => true,
+    'no_empty_comment'                   => false,
+    'no_superfluous_elseif'              => true,
+    'no_superfluous_phpdoc_tags'         => true,
+    'not_operator_with_successor_space'  => true,
+    'no_unset_on_property'               => true,
 
     'ordered_imports' => [
         'imports_order'  => [ 'class', 'function', 'const' ],
         'sort_algorithm' => 'alpha',
     ],
 
+    'phpdoc_add_missing_param_annotation' => false,
+    'phpdoc_annotation_without_dot'       => true,
+    'phpdoc_to_comment'                   => true,
+
+    'return_assignment' => true,
+
+    'single_line_comment_style' => [ 'comment_types' => [ 'hash' ] ],
+
+    'strict_comparison' => true,
+
     'void_return' => false,
+
+    'yoda_style' => [
+        'equal'            => false,
+        'identical'        => false,
+        'less_and_greater' => false,
+    ],
 ];
