a9db92e98a3704cc24ef0c0222e1b164061f1df6
moeetomoni <dev@moee.fr>
2023-03-11 16:35:19 +0100

    8-refactor(factory): return type MUST be self

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare( strict_types=1 );
 
 namespace Database\Factories;
 
@@ -27,26 +27,26 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'two_factor_secret' => null,
+            'name'                      => $this->faker->name(),
+            'email'                     => $this->faker->unique()->safeEmail(),
+            'email_verified_at'         => now(),
+            'password'                  => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'two_factor_secret'         => null,
             'two_factor_recovery_codes' => null,
-            'remember_token' => Str::random(10),
-            'profile_photo_path' => null,
-            'current_team_id' => null,
+            'remember_token'            => Str::random(10),
+            'profile_photo_path'        => null,
+            'current_team_id'           => null,
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return $this
+     * @return self
      */
-    public function unverified(): static
+    public function unverified(): self
     {
-        return $this->state(function (array $attributes) {
+        return $this->state(function (array $attributes): array {
             return [
                 'email_verified_at' => null,
             ];
@@ -56,18 +56,18 @@ class UserFactory extends Factory
     /**
      * Indicate that the user should have a personal team.
      *
-     * @return $this
+     * @return self
      */
-    public function withPersonalTeam(): static
+    public function withPersonalTeam(): self
     {
-        if (! Features::hasTeamFeatures()) {
+        if (!Features::hasTeamFeatures()) {
             return $this->state([]);
         }
 
         return $this->has(
             Team::factory()
                 ->state(function (array $attributes, User $user) {
-                    return ['name' => $user->name . '\'s Team', 'user_id' => $user->id, 'personal_team' => true];
+                    return [ 'name' => $user->name . '\'s Team', 'user_id' => $user->id, 'personal_team' => true ];
                 }),
             'ownedTeams'
         );
