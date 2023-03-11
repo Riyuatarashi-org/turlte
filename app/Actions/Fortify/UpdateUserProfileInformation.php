103ea514c3aa5a8ef315c3b7b2cc9dbf58308d06
moeetomoni <dev@moee.fr>
2023-03-11 16:16:52 +0100

    6-refactor(action): increase size limit for profile pictures

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare( strict_types=1 );
 
 namespace App\Actions\Fortify;
 
@@ -15,14 +15,14 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     /**
      * Validate and update the given user's profile information.
      *
-     * @param array<string, string> $input
+     * @param  array<string, string>  $input
      */
     public function update(User $user, array $input): void
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
-            'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
+            'name'  => [ 'required', 'string', 'max:255' ],
+            'email' => [ 'required', 'email', 'max:255', Rule::unique('users')->ignore($user->id) ],
+            'photo' => [ 'nullable', 'mimes:jpg,jpeg,png', 'max:2048' ],
         ])->validateWithBag('updateProfileInformation');
 
         if (isset($input['photo'])) {
@@ -34,7 +34,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $this->updateVerifiedUser($user, $input);
         } else {
             $user->forceFill([
-                'name' => $input['name'],
+                'name'  => $input['name'],
                 'email' => $input['email'],
             ])->save();
         }
@@ -43,13 +43,13 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     /**
      * Update the given verified user's profile information.
      *
-     * @param array<string, string> $input
+     * @param  array<string, string>  $input
      */
     protected function updateVerifiedUser(User $user, array $input): void
     {
         $user->forceFill([
-            'name' => $input['name'],
-            'email' => $input['email'],
+            'name'              => $input['name'],
+            'email'             => $input['email'],
             'email_verified_at' => null,
         ])->save();
 
