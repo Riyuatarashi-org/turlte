ed4e3788a52185c543def54ed7f480bbdc18d077
moeetomoni <dev@moee.fr>
2023-03-12 02:10:15 +0100

    11-refactor(test): retake phpunit test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,22 +1,15 @@
 <?php
 
-declare(strict_types = 1);
-
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-/**
- * @internal
- *
- * @coversNothing
- */
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function testThatTrueIsTrue()
+    public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
     }
