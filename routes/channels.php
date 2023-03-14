2334dbc2309d204fc2411bd5b2571ae5e0426cd2
moeetomoni <dev@moee.fr>
2023-03-14 17:17:54 +0100

    8-refactor(route): typing

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -9,12 +10,12 @@ use Illuminate\Support\Facades\Broadcast;
 | Broadcast Channels
 |--------------------------------------------------------------------------
 |
-| Here you may register all of the event broadcasting channels that your
+| Here you may register all the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('App.Models.User.{id}', function (User $user, int $id) {
+    return $user->id === $id;
 });
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+use Illuminate\Console\Command;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
+$inspireClosure = Closure::bind(function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+}, new Command());
+
+Artisan::command('inspire', $inspireClosure)
+    ->purpose('Display an inspiring quote');
